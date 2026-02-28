a03e477e67db5d68d06b06361108f92abf06c84e
Carol Savchick <carolmariehinds@gmail.com>
2026-02-28 13:09:03 -0500

    part3 updates

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,19 @@
+async function getRandomImage() {
+    const client_id = "";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
 "use strict"
 
 const elements = {
