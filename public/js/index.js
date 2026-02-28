e25efa918e3f98d57724a3c770c11b107dc961ef
Carol Savchick <carolmariehinds@gmail.com>
2026-02-28 15:07:49 -0500

    part 4 update, also fixed missing function call

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,9 @@
 async function getRandomImage() {
-    const client_id = "";
-    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    const endpoint = "http://localhost:8080/api/v1/getRandomImage";
     try {
         const response = await fetch(endpoint);
         const returnedData = await response.json();
-        const receivedPhotoUrl = returnedData.urls.regular;
+        const receivedPhotoUrl = returnedData.data;
 
         const imgDiv = document.querySelector(".background-img");
         imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
@@ -13,6 +12,7 @@ async function getRandomImage() {
     }
 }
 
+getRandomImage();
 
 "use strict"
 
